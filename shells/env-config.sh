b9ede0ac5e0b3c45cf1718b094fd8457db542fdd
dyadyaJora <ja_massa97@mail.ru>
2019-01-29 14:31:23 +0300

    Create env-config.sh

---
+++ shells/env-config.sh
@@ -0,0 +1,12 @@
+# For different .env files in each folders
+envfile=".env.filename"
+
+function pathedit {
+  str=$0
+  str=${str:0:(-11)}
+  cp $0 "$0.env"
+}
+
+export -f pathedit
+
+find . -name $envfile -exec bash -c "pathedit $0" {} \;
